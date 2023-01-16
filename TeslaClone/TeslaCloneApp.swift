07b1febca10ea0120d16295933e6f82c30ca7dcf
Sunehar Sandhu <sunehar.sandhu@gmail.com>
2023-01-16 10:23:13 -0600

    Initial Commit

---
+++ TeslaClone/TeslaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaCloneApp.swift
+//  TeslaClone
+//
+//  Created by Sunehar Sandhu on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TeslaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
